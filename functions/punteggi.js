f8dbdc20364a64272436c1cb886bbb48cead391b
Ranallo94 <andrea.ranalli.94@gmail.com>
2026-06-08 12:31:03 +0200

    Add files via upload

--- functions/punteggi.js
+++ functions/punteggi.js
@@ -120,7 +120,7 @@ function calcolaPunteggio(pronostici, risultati) {
       if (!rMatch || !rMatch.modalita) return;
       const pMatch = pFase[matchId];
       if (!pMatch) return;
-      if (pMatch.modalita === rMatch.modalita && pMatch.vincitore === rMatch.vincitore) {
+      if (pMatch.modalita === rMatch.modalita) {
         bd.modalita.punti += REG.fasi_eliminatorie.modalita_passaggio_turno.punti;
         bd.modalita.corretti++;
       }
